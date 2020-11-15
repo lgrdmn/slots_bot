4917ff957bc0710a2a5359b72e39407a536631ee
lgrdmn <mv_tobletka@mail.ru>
2020-11-15 18:42:42 +0500

    db error

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
-from slots import creator_db, up_balance, start_values, all_players, game
+from slots import creator_db, up_balance, start_values, all_players, game, player_finder
 
 
 def start(update, context):
     buttons = [[KeyboardButton(text='spin 🎰'), KeyboardButton(text='bet 💲')],
                [KeyboardButton(text='credit 💰'), KeyboardButton(text='set_name')]]
     keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
-    update.message.reply_text('Добро пожаловать в казино-бота', reply_markup=keyboard)
-    creator_db()
+    update.message.reply_text('Добро пожаловать в казино-бота!', reply_markup=keyboard)
     start_values(update.message.chat.id)
 
 
@@ -17,32 +16,38 @@ def spin(update, context):
         buttons = [[KeyboardButton(text='spin 🎰'), KeyboardButton(text='bet 💲')],
                    [KeyboardButton(text='credit 💰'), KeyboardButton(text='set_name')]]
         keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
-        # game(update.message.chat.id)
-        slot1, slot2, slot3, win_size, player_id, game_credit, bet_size = game(update.message.chat.id)
-        if game_credit > 0:
+        results = player_finder(update.message.chat.id)
+        player_credit = results[0][1]
+        if player_credit <= 0:
+            update.message.reply_text(f'Пожалуйста пополните баланс!'
+                                      f'\nБаланс: {player_credit}'
+                                      , reply_markup=keyboard)
+        else:
+            slot1, slot2, slot3, win_size, player_id, game_credit, bet_size = game(update.message.chat.id)
             update.message.reply_text(f'[{slot1}][{slot2}][{slot3}]'
                                       f'\nВыигрыш: {win_size}'
                                       f'\nБаланс: {game_credit}'
                                       f'\nРазмер ставки: {bet_size}'
-                                      , reply_markup=keyboard)
-        else:
-            update.message.reply_text(f'Пожалуйста пополните баланс'
-                                      f'\nБаланс: {game_credit}'
                                       , reply_markup=keyboard)
     except Exception as error:
         print(error)
 
 
 def credit(update, context):
-    buttons = [[KeyboardButton(text='spin 🎰'), KeyboardButton(text='bet 💲')],
-               [KeyboardButton(text='credit 💰'), KeyboardButton(text='set_name')]]
-    keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
-    credit_size, bet_size = up_balance(update.message.chat.id)
-    update.message.reply_text(f'Ваш баланс пополнен'
-                              f'\nБаланс: {credit_size}'
-                              f'\nРазмер ставки: {bet_size}'
-                              , reply_markup=keyboard)
+    try:
+        buttons = [[KeyboardButton(text='spin 🎰'), KeyboardButton(text='bet 💲')],
+                   [KeyboardButton(text='credit 💰'), KeyboardButton(text='set_name')]]
+        keyboard = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+        credit_size, bet_size = up_balance(update.message.chat.id)
+        update.message.reply_text(f'Ваш баланс пополнен'
+                                  f'\nБаланс: {credit_size}'
+                                  f'\nРазмер ставки: {bet_size}'
+                                  , reply_markup=keyboard)
+    except Exception as error:
+        print(error)
 
+
+creator_db()
 
 updater = Updater("1462029373:AAExm0dW7OTsyODp4IqA2qgTSkYuHIXaDvg", use_context=True)
 
@@ -51,8 +56,11 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(MessageHandler(filters=Filters.regex('spin 🎰'), callback=spin))
 dispatcher.add_handler(MessageHandler(filters=Filters.regex('credit 💰'), callback=credit))
+dispatcher.add_handler(MessageHandler(filters=Filters.regex('bet 💲'), callback=credit))
 
 updater.start_polling()
+
+print(all_players())
 
 
 
--- slots.py
+++ slots.py
@@ -29,14 +29,17 @@ def creator_db():
 
 def start_values(start_id):
     """ Данные для нового игрока """
-    conn = sqlite3.connect('slots_players.db')
-    cur = conn.cursor()
-    start_credit = 1500
-    start_bet_size = 1
-    values = [start_id, start_credit, start_bet_size]
-    cur.execute("INSERT INTO players VALUES(?, ?, ?);", values)
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('slots_players.db')
+        cur = conn.cursor()
+        start_credit = 40
+        start_bet_size = 4
+        values = [start_id, start_credit, start_bet_size]
+        cur.execute("INSERT INTO players VALUES(?, ?, ?);", values)
+        conn.commit()
+        conn.close()
+    except Exception as error:
+        print(error)
 
 
 def all_players():
@@ -51,35 +54,44 @@ def all_players():
 
 def add_values_to_db(player_id, add_credit, add_bet_size):
     """ Запись результата игры в базу данных игроков """
-    conn = sqlite3.connect('slots_players.db')
-    cur = conn.cursor()
-    cur.execute("UPDATE players SET 'id'=?, 'credit'=?, 'bet_size'=?;",
-                (player_id, add_credit, add_bet_size))
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect('slots_players.db')
+        cur = conn.cursor()
+        cur.execute("UPDATE players SET 'credit'=?, 'bet_size'=? WHERE 'id'=?;",
+                    (add_credit, add_bet_size, player_id))
+        conn.commit()
+        conn.close()
+    except Exception as error:
+        print(error)
 
 
 def up_balance(player_id):
     """ Пополнение баланса после обнуления """
-    up_credit = 100
-    up_bet_size = 5
-    conn = sqlite3.connect('slots_players.db')
-    cur = conn.cursor()
-    cur.execute("UPDATE players SET 'id'=?, 'credit'=?, 'bet_size'=?;",
-                (player_id, up_credit, up_bet_size))
-    conn.commit()
-    conn.close()
-    return up_credit, up_bet_size
+    try:
+        up_credit = 100
+        up_bet_size = 5
+        conn = sqlite3.connect('slots_players.db')
+        cur = conn.cursor()
+        cur.execute("UPDATE players SET 'credit'=?, 'bet_size'=? WHERE 'id'=?;",
+                    (up_credit, up_bet_size, player_id))
+        conn.commit()
+        conn.close()
+        return up_credit, up_bet_size
+    except Exception as error:
+        print(error)
 
 
 def player_finder(player_id):
     """ Поиск данных игрока по id """
-    conn = sqlite3.connect('slots_players.db')
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM players WHERE id=?;", (player_id,))
-    results = cur.fetchall()
-    conn.close()
-    return results
+    try:
+        conn = sqlite3.connect('slots_players.db')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM players WHERE id=?;", (player_id,))
+        results = cur.fetchall()
+        conn.close()
+        return results
+    except Exception as error:
+        print(error)
 
 
 def values_generator():
@@ -98,13 +110,18 @@ def bonus(first_value, second_value, third_value):
 
 def game(player_id):
     results = player_finder(player_id)
+    print(results)
     game_credit = results[0][1]
     game_bet_size = results[0][2]
     slot1, slot2, slot3 = values_generator()
     bonus_size = bonus(slot1, slot2, slot3)
+    print(game_credit, game_bet_size)
     game_credit -= game_bet_size
+    print(game_credit, game_bet_size)
     win_size = bonus_size * game_bet_size
     game_credit += win_size
+    print(game_credit, win_size)
+    print(player_id, game_credit, game_bet_size)
     add_values_to_db(player_id, game_credit, game_bet_size)
     return slot1, slot2, slot3, win_size, player_id, game_credit, game_bet_size
 
